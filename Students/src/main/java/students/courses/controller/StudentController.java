9c9d81c231e9ea10820e01d1e0e869f3d41026bb
JoacimStahl96 <69908677+JoacimStahl96@users.noreply.github.com>
2021-04-13 17:19:55 +0200

    added cors and backend validation

--- Students/src/main/java/students/courses/controller/StudentController.java
+++ Students/src/main/java/students/courses/controller/StudentController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("students") // localhost:8080/students
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     private final StudentService studentService;
--- Students/src/main/java/students/courses/service/impl/StudentServiceImpl.java
+++ Students/src/main/java/students/courses/service/impl/StudentServiceImpl.java
@@ -3,7 +3,6 @@ package students.courses.service.impl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import students.courses.Util.Utility;
 import students.courses.dto.StudentDto;
@@ -31,9 +30,9 @@ public class StudentServiceImpl implements StudentService {
 
         Iterable<StudentEntity> studentEntities = studentRepository.findAll();
         ArrayList<StudentDto> studentDtos = new ArrayList<>();
-        for (StudentEntity studentEntity : studentEntities){
+        for (StudentEntity studentEntity : studentEntities) {
             StudentDto studentDto = new StudentDto();
-            BeanUtils.copyProperties(studentEntity,studentDto);
+            BeanUtils.copyProperties(studentEntity, studentDto);
             studentDtos.add(studentDto);
         }
         return studentDtos;
@@ -45,22 +44,27 @@ public class StudentServiceImpl implements StudentService {
         Optional<StudentEntity> studentIdEntity = studentRepository.findByStudentId(studentId);
         return studentIdEntity.map(studentEntity -> {
             StudentDto studentDto = new StudentDto();
-            BeanUtils.copyProperties(studentEntity,studentDto);
+            BeanUtils.copyProperties(studentEntity, studentDto);
             return studentDto;
         });
     }
 
     public StudentDto createStudent(StudentDto studentDetails) {
+            // age is also validated in frontend, better safe than sorry
+        if (studentDetails.getName().equals("") || studentDetails.getLastName().equals("") || studentDetails.getAge() < 5) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 
+        }
         StudentEntity studentEntity = new StudentEntity();
-        BeanUtils.copyProperties(studentDetails,studentEntity);
+
+        BeanUtils.copyProperties(studentDetails, studentEntity);
         //Generate a hashCode as student_id
         String studentId = utility.generateHashedStudentId(studentDetails.getName());
         studentEntity.setStudentId(studentId.substring(3));
         //Save new data-->
         StudentEntity studentEntityOut = studentRepository.save(studentEntity);
         StudentDto studentDtoOut = new StudentDto();
-        BeanUtils.copyProperties(studentEntityOut,studentDtoOut);
+        BeanUtils.copyProperties(studentEntityOut, studentDtoOut);
 
         return studentDtoOut;
     }
@@ -73,16 +77,16 @@ public class StudentServiceImpl implements StudentService {
 
         return studentIdEntity.map(studentEntity -> {
             StudentDto response = new StudentDto();
-            studentEntity.setStudentId(studentDto.getStudentId()!=null ? utility.generateHashedStudentId(studentDto.getName()).substring(3) : studentEntity.getStudentId());
-            studentEntity.setName(studentDto.getName()!=null ? studentDto.getName() : studentEntity.getName());
-            studentEntity.setLastName(studentDto.getLastName()!=null ? studentDto.getLastName() : studentEntity.getLastName());
+            studentEntity.setStudentId(studentDto.getStudentId() != null ? utility.generateHashedStudentId(studentDto.getName()).substring(3) : studentEntity.getStudentId());
+            studentEntity.setName(studentDto.getName() != null ? studentDto.getName() : studentEntity.getName());
+            studentEntity.setLastName(studentDto.getLastName() != null ? studentDto.getLastName() : studentEntity.getLastName());
 
-            studentEntity.setPresent(studentDto.isPresent() != studentEntity.isPresent()  ? studentDto.isPresent() : studentEntity.isPresent());
+            studentEntity.setPresent(studentDto.isPresent() != studentEntity.isPresent() ? studentDto.isPresent() : studentEntity.isPresent());
 
-            studentEntity.setAge(studentDto.getAge()!=null ? studentDto.getAge() : studentEntity.getAge());
+            studentEntity.setAge(studentDto.getAge() != null ? studentDto.getAge() : studentEntity.getAge());
 
             StudentEntity updatedStudentEntity = studentRepository.save(studentEntity);
-            BeanUtils.copyProperties(updatedStudentEntity,response);
+            BeanUtils.copyProperties(updatedStudentEntity, response);
 
             return response;
         });
@@ -93,7 +97,7 @@ public class StudentServiceImpl implements StudentService {
     public void deleteStudent(String studentId) {
         Optional<StudentEntity> studentExists = studentRepository.findByStudentId(studentId);
 
-        if (studentExists.isEmpty()){
+        if (studentExists.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Student doesn't exists");
         }
         studentRepository.delete(studentExists.get());
