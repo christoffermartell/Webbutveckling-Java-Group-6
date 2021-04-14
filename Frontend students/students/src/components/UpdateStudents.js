6faa6d83900ebe346f7910fea819ffb3698fc366
JoacimStahl96 <69908677+JoacimStahl96@users.noreply.github.com>
2021-04-14 19:43:43 +0200

    Everything implemented BUT
    Vi har lite osmidigt med delete metoderna där url är annorlunda, jag klarade inte riktigt av att få till det på det sättet som Nikola gjort... så vi har ju nu 2 stycken delete metoder.

--- Frontend students/students/src/components/UpdateStudents.js
+++ Frontend students/students/src/components/UpdateStudents.js
@@ -1,22 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UpdateStudents = (props) => {
+	const { name, last_name, age, present, student_id } = props.specificStudent;
+
+	const [studentName, setStudentName] = useState(name);
+	const [studentlastName, setStudentLastName] = useState(last_name);
+	const [studentAge, setStudentAge] = useState(age);
+	const [studentPresent, setStudentPresent] = useState(present);
+	const studentId = student_id;
+
 	const clickHandler = () => {
 		props.setView("startPage");
 	};
+	const editHandler = () => {
+		props.updateStudent(
+			studentId,
+			studentName,
+			studentlastName,
+			studentAge,
+			studentPresent
+		);
+	};
 	return (
 		<div className="wrapper">
-			<form>
-				<h2 onClick={() => clickHandler()}>Här kan du ändra eller radera studenter</h2>
-				<div>
-					<span className="labelTextInSpan">
-						<label>Name</label>
-					</span>
-					<input id="editName" type="text" />
+			<form onSubmit={() => editHandler()}>
+				<h2 id="updateText" onClick={() => clickHandler()}>
+					Edit or delete Student
+				</h2>
+				<div className="divCreateStudent">
+					<label>
+						Name{" "}
+						<input
+							id="editName"
+							type="text"
+							placeholder="Name"
+							onChange={(e) => setStudentName(e.target.value)}
+							value={studentName}
+						/>
+					</label>
 				</div>
-
-				<label>Last name</label>
-				<input id="editlastName" type="text" />
+				<div className="divCreateStudent">
+					<label>
+						Last name{" "}
+						<input
+							id="editlastName"
+							type="text"
+							placeholder="Last name"
+							onChange={(e) => setStudentLastName(e.target.value)}
+							value={studentlastName}
+						/>
+					</label>
+				</div>
+				<div className="divCreateStudent">
+					<label>
+						Age{" "}
+						<input
+							id="editlastName"
+							type="number"
+							min="5"
+							onChange={(e) => setStudentAge(e.target.value)}
+							value={studentAge}
+						/>
+					</label>
+				</div>
+				<label htmlFor="present">
+					<input
+						type="checkbox"
+						id="present"
+						name="present"
+						onChange={() => setStudentPresent(!studentPresent)}
+						defaultChecked={studentPresent}
+					></input>
+					Present
+				</label>
+				<button className="btn" id="editBtn" type="submit">
+					Edit
+				</button>
+				<button
+					className="btn"
+					id="deleteBtn"
+					onClick={() => props.deleteStudentInEdit(studentId)}
+				>
+					Delete
+				</button>
 			</form>
 		</div>
 	);
